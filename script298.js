2c6b3232daca5e511fe98988f0c24388d3219832
Diegobrizuela03 <140198071+Diegobrizuela03@users.noreply.github.com>
2023-09-03 18:41:29 -0300

    Add files via upload

---
+++ script298.js
@@ -0,0 +1,26 @@
+var nombre = prompt("Por favor ingrese su nombre:")
+
+    function saludo(){
+
+    if (nombre){
+
+        alert ("Bienvenido/a" + " " + nombre);
+        
+    
+    }else{
+
+        alert ("No ingresaste ningún nombre");
+    }
+    }
+    console.log(nombre);
+
+saludo();
+
+
+
+
+
+
+
+
+
